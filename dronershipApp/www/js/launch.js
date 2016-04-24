d42c702dfc2bbd7293a583c9e277225091041e5a
davidkatz <15Dkatz@shcp.edu>
2016-04-24 02:37:17 -0700

    Added final response pages

--- dronershipApp/www/js/launch.js
+++ dronershipApp/www/js/launch.js
@@ -1,6 +1,32 @@
 myApp.controller('LaunchCtrl', function($scope, $rootScope) {
 	$scope.backgroundStyle = "clear";
 
-	$scope.icon = "ion-checkmark-circled";
+	$scope.icon = "";
+
+	$rootScope.$on("changeLbackground", function() {
+		switch($rootScope.launchStatus) {
+			case "Clear for takeoff!":
+				$scope.backgroundStyle = "clear";
+				$scope.icon = "ion-checkmark-circled";
+				$scope.iconColor = "dark";
+				break;
+
+			case "No-Go":
+				$scope.backgroundStyle = "no-go";
+				$scope.icon = "ion-close-circled";
+				$scope.iconColor = "light";
+				break;
+			case "Very risky":
+				$scope.backgroundStyle = "very-risky";
+				$scope.icon = "ion-alert-circled";
+				$scope.iconColor = "light";
+				break;
+			case "A little risky":
+				$scope.backgroundStyle = "little-risky";
+				$scope.icon = "ion-alert-circled";
+				$scope.iconColor = "dark";
+				break;
+		}
+	})
 
 })
