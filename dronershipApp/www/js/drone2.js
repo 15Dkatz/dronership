9524816e85375ed6883b066b7b0f4c9041b43f34
Kalem <ksio2123@gmail.com>
2016-04-28 13:41:07 -0700

    modified drone2.js for withinFlyzone function

--- dronershipApp/www/js/drone2.js
+++ dronershipApp/www/js/drone2.js
@@ -1,311 +1,370 @@
-// myApp.controller('DroneCtrl', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform, $http, $rootScope) {
+myApp.controller('DroneCtrl', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform, $http, $rootScope) {
 
 
-//   	$scope.test = "testing $scope, $scope works| MAP EXAMPLE";
+  	$scope.test = "testing $scope, $scope works| MAP EXAMPLE";
 
-//   	$scope.long;
-//   	$scope.lat;
+  	$scope.long;
+  	$scope.lat;
 
-//   	$scope.showInfo = false;
+  	$scope.showInfo = false;
 
-//   	// get Information method
-//   	$scope.myDrone = {
-//   		flightTime: 0,
-//   		weight: 0,
-//   		operatingRange: 0
-//   	};
+  	// get Information method
+  	$scope.myDrone = {
+  		flightTime: 0,
+  		weight: 0,
+  		operatingRange: 0
+  	};
 
-//   	$scope.postPrefix = "";
+  	$scope.postPrefix = "";
 
-//   	$scope.calculateLaunchStatus = function(flightTime, weight, operatingRange) {
-//   		$scope.postPrefix = "Re-";
-//   		$scope.showInfo = true;
-//   		// unccomment grabLocation section for the location info
-//   		// Grab Location **********
-//   		ionic.Platform.ready(function(){
-// 	    // Code goes here
-//     	$ionicLoading.show({
-//             template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
-//         });
+  	$scope.calculateLaunchStatus = function(flightTime, weight, operatingRange, latitudeInput, longitudeInput) {
+  		$scope.postPrefix = "Re-";
+  		$scope.showInfo = true;
+  		// unccomment grabLocation section for the location info
+  		// Grab Location **********
+  		ionic.Platform.ready(function(){
+	    // Code goes here
+    	$ionicLoading.show({
+            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+        });
          
-//         var posOptions = {
-//             enableHighAccuracy: true,
-//             timeout: 20000,
-//             maximumAge: 0
-//         };
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
  		
-//  		$cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-//             var lat  = position.coords.latitude;
-//             var long = position.coords.longitude;
-//             var myLatlng = new google.maps.LatLng(lat, long);
-//             var mapOptions = {
-//                 center: myLatlng,
-//                 zoom: 16,
-//                 mapTypeId: google.maps.MapTypeId.ROADMAP
-//             };          
-             
-//             var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
-             
-//             $scope.map = map;   
-//             $ionicLoading.hide();           
+ 		$cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+            // position.coords.direction
 
-//             console.log(lat, long, map);
+            $ionicLoading.hide();           
 
-//             // testing latitudes
-//             $scope.lat = lat;
-//             $scope.long = long;
+            $scope.lat = lat;
+            $scope.long = long;
 
-//             $rootScope.latitude = $scope.lat;
-//             $rootScope.longitude = $scope.long;
+            if (operatingRange!=undefined) {
+				$scope.myDrone.operatingRange = operatingRange;
+			}
 
-//             console.log("flightTime", flightTime, "weight", weight, "operatingRange", operatingRange);
+	  		if (latitudeInput) {
+	  			console.log("latitudeInput seen", latitudeInput);
+	  			$scope.lat = latitudeInput;
+	  		}
 
-//             // $scope.myDrone.flightTime = 0;
-// 			if (flightTime!=undefined) {
-// 				$scope.myDrone.flightTime = flightTime;
-// 			}
-
-// 			// $scope.myDrone.weight = 0;
-// 			if (weight!=undefined) {
-// 				$scope.myDrone.weight = weight;
-// 			}
-			
-// 			// $scope.myDrone.operatingRange = 0;	
-// 			if (operatingRange!=undefined) {
-// 				$scope.myDrone.operatingRange = operatingRange;
-// 			}
+	  		if (longitudeInput) {
+	  			$scope.long = longitudeInput;
+	  		}
 
 
+            $rootScope.latitude = $scope.lat;
+            $rootScope.longitude = $scope.long;
 
+            // for coordinates section
+            $scope.latitudePlaceholder = ($rootScope.latitude).toPrecision(6);
+            $scope.longitudePlaceholder = ($rootScope.longitude).toPrecision(6);
 
-//             getWunderGround();
+			if (flightTime!=undefined) {
+				$scope.myDrone.flightTime = flightTime;
+			}
+
+			// $scope.myDrone.weight = 0;
+			if (weight!=undefined) {
+				$scope.myDrone.weight = weight;
+			}
+
+            getWunderGround();
 
             
-// 	        }, function(err) {
-// 	            $ionicLoading.hide();
-// 	            console.log(err);
-// 	        });
+	        }, function(err) {
+	            $ionicLoading.hide();
+	            console.log(err);
+	        });
 	        
-// 		})  
+		})  
 
 
-//   		$scope.calculateStatus();
 
-//   	}
+  		// $scope.calculateStatus();
 
-
-//   	$scope.data;
-//   	// available data: *****
-//   	// weather
-//   	// temperature_string
-//   	// relative_humidity
-//   	// wind_string
-//   	// wind_mph
-//   	// wind_gust_mph
-//   	// observation_location.full
-
-//   	$scope.weather;
-//   	$scope.wind_mph;
-//   	$scope.wind_gust_mph;
-//   	$scope.relative_humidity;
-//   	$scope.temp_f;
+  	}
 
 
-//   	var getWunderGround = function() {
+  	$scope.data;
+  	// available data: *****
+  	// weather
+  	// temperature_string
+  	// relative_humidity
+  	// wind_string
+  	// wind_mph
+  	// wind_gust_mph
+  	// observation_location.full
 
-//   		// uncomment to call API! ******************
-// 		var getString = 'http://api.wunderground.com/api/06dfbc32cb33068b/conditions/q/' + $scope.lat + ',' + $scope.long + '.json';
-
-//     	$http({
-// 	        method : "GET",
-// 	        url : getString
-// 	    }).then(function mySuccess(response) {
-// 	        // $scope.myWelcome = response.data;
-// 	        console.log(response.data);
-// 	        $scope.data = response.data.current_observation;
+  	$scope.weather;
+  	$scope.wind_mph;
+  	$scope.wind_gust_mph;
+  	$scope.relative_humidity;
+  	$scope.temp_f;
 
 
-// 	        $scope.weather = $scope.data.weather;
-// 		  	$scope.wind_mph = $scope.data.wind_mph;
-// 		  	$scope.wind_gust_mph = $scope.data.wind_gust_mph;
-// 		  	$scope.relative_humidity = $scope.data.relative_humidity;
-// 		  	$scope.temp_f = $scope.data.temp_f;
-// 		  	console.log("weather", $scope.weather, "wind_mph", $scope.wind_mph, "gust", $scope.wind_gust_mph, "humidity", $scope.relative_humidity, "temperature", $scope.temp_f);
+  	var getWunderGround = function() {
 
-// 		  	$scope.calculateStatus();
-// 	        // 
+  		// uncomment to call API! ******************
+		var getString = 'http://api.wunderground.com/api/06dfbc32cb33068b/conditions/q/' + $scope.lat + ',' + $scope.long + '.json';
 
-// 	    }, function myError(response) {
-// 	        // $scope.myWelcome = response.statusText;
-// 	        console.log(response.statusText);
-// 	    });
-// 	 	// ******************
+    	$http({
+	        method : "GET",
+	        url : getString
+	    }).then(function mySuccess(response) {
+	        // $scope.myWelcome = response.data;
+	        console.log(response.data);
+	        $scope.data = response.data.current_observation;
+
+
+	        $scope.weather = $scope.data.weather;
+		  	$scope.wind_mph = $scope.data.wind_mph;
+		  	$scope.wind_gust_mph = $scope.data.wind_gust_mph;
+		  	$scope.relative_humidity = $scope.data.relative_humidity;
+		  	$scope.temp_f = $scope.data.temp_f;
+		  	// console.log("weather", $scope.weather, "wind_mph", $scope.wind_mph, "gust", $scope.wind_gust_mph, "humidity", $scope.relative_humidity, "temperature", $scope.temp_f);
+
+		  	$scope.calculateStatus();
+	        // 
+
+	    }, function myError(response) {
+	        // $scope.myWelcome = response.statusText;
+	        console.log(response.statusText);
+	    });
+	 	// ******************
 	 	
 
-// 	 	$rootScope.$broadcast('mapCentered');
+	 	$rootScope.$broadcast('mapCentered');
 
-// 	}
+	}
 
-// 	$scope.status = 1;
+	$scope.status = 1;
 
-// 	$scope.myDrone;
+	$scope.myDrone;
 
-// 	$scope.showOtherDroneForm = false;
+	$scope.showOtherDroneForm = false;
 
-// 	$scope.selectedDrone = function(myDrone) {
-// 		console.log("myDrone", myDrone);
-// 		switch (myDrone) {
-// 			// most common drones
-// 			case "DJI Phantom 4":
-// 				$scope.showOtherDroneForm = false;
-// 				$scope.myDrone = {
-// 					flightTime: 28, //minutes
-// 					operatingRange: 5000, //operating range
-// 					weight: 1380 //grams
-// 				}
+	$scope.selectedDrone = function(myDrone) {
+		console.log("myDrone", myDrone);
+		switch (myDrone) {
+			// most common drones
+			case "DJI Phantom 4":
+				$scope.showOtherDroneForm = false;
+				$scope.myDrone = {
+					flightTime: 28, //minutes
+					operatingRange: 5000, //operating range
+					weight: 1380 //grams
+				}
 
-// 				break;
-// 			case "AEE Toruk AP10":
-// 				$scope.showOtherDroneForm = false;
-// 				$scope.myDrone = {
-// 					flightTime: 25, //minutes
-// 					operatingRange: 500, //operating range
-// 					weight: 1300 //grams
-// 				}
-// 				break;
-// 			// Flypro-XEagle-Sport
-// 			case "Flypro XEagle Sport":
-// 				$scope.showOtherDroneForm = false;
-// 				$scope.myDrone = {
-// 					flightTime: 22,
-// 					operatingRange: 300,
-// 					weight: 1260
+				break;
+			case "AEE Toruk AP10":
+				$scope.showOtherDroneForm = false;
+				$scope.myDrone = {
+					flightTime: 25, //minutes
+					operatingRange: 500, //operating range
+					weight: 1300 //grams
+				}
+				break;
+			// Flypro-XEagle-Sport
+			case "Flypro XEagle Sport":
+				$scope.showOtherDroneForm = false;
+				$scope.myDrone = {
+					flightTime: 22,
+					operatingRange: 300,
+					weight: 1260
 
-// 				}
-// 				break;
-// 			case "Autel Robotics X-Star Premium":
-// 				$scope.showOtherDroneForm = false;
-// 				$scope.myDrone = {
-// 					flightTime: 25,
-// 					operatingRange: 2000,
-// 					weight: 1420
+				}
+				break;
+			case "Autel Robotics X-Star Premium":
+				$scope.showOtherDroneForm = false;
+				$scope.myDrone = {
+					flightTime: 25,
+					operatingRange: 2000,
+					weight: 1420
 
-// 				}
-// 				break;
-// 			case "ProDrone Byrd Premium":
-// 				$scope.showOtherDroneForm = false;
-// 				$scope.myDrone = {
-// 					flightTime: 29,
-// 					operatingRange: 2000,
-// 					weight: 1890
+				}
+				break;
+			case "ProDrone Byrd Premium":
+				$scope.showOtherDroneForm = false;
+				$scope.myDrone = {
+					flightTime: 29,
+					operatingRange: 2000,
+					weight: 1890
 
-// 				}
-// 				break;
-// 			case "Not Listed":
-// 				$scope.showOtherDroneForm = true;
-// 				// default drone specs;
-// 				$scope.myDrone = {
-// 					// flightTime: 0, //minutes
-// 					// operatingRange: 0, //operating range
-// 					// weight: 0 //grams
-// 				}
-
-// 			default:
-// 				break;
-
-
-// 		}
-// 		console.log("drone Object", $scope.myDrone);
-
-// 	}
-
-// 	$scope.flightTime;
-// 	$scope.weight;
-// 	$scope.operatingRange;
+				}
+				break;
+			case "I have a different drone":
+				console.log($scope.showOtherDroneForm);
+				$scope.showOtherDroneForm = true;
+				// default drone specs;
+				$scope.myDrone = {
+					// flightTime: 0, //minutes
+					// operatingRange: 0, //operating range
+					// weight: 0 //grams
+				}
+				break;
+			// case "Check conditions without a drone":
+				
+			// 	break;
+			default:
+				$scope.showOtherDroneForm = false;
+				break;
 
 
-// 	$scope.calculateStatus = function() {
-// 		var windMphLimit = 30;
-// 		var windGustLimit = 30;
-// 		var humidityLimit = 100;
-// 		var humidityModifier = .05;
+		}
+		console.log("drone Object", $scope.myDrone);
 
-// 		$scope.status = 1;
+	}
 
-// 		// Modify the following to implement test conditions **************
-// 		// $scope.weather = "Partly Cloudy";
-// 		// $scope.wind_mph = 1.8;
-// 		// $scope.wind_gust_mph = "10.5";
-// 		// $scope.relative_humidity = "64%";
-// 		// $scope.temp_f = 63.1;
-// 		// console.log("weather", $scope.weather, "wind_mph", $scope.wind_mph, "gust", $scope.wind_gust_mph, "humidity", $scope.relative_humidity, "temperature", $scope.temp_f);
+	$scope.flightTime;
+	$scope.weight;
+	$scope.operatingRange;
 
-// 		var windMphDanger = $scope.wind_mph/windMphLimit;
-// 		$scope.status -= windMphDanger;
-// 		// console.log("resulting Status", $scope.status);
+	$scope.changeLatLon = function(latitude, longitude) {
+		$rootScope.latitude = latitude;
+		$rootScope.longitude = longitude;
+	 // $scope.myData = [10,20,30,40,60, 80, 20, 50];
+	}
 
-// 		var windGustDanger = parseFloat($scope.wind_gust_mph)/windGustLimit;
-// 		$scope.status -= windGustDanger;
-// 		// console.log("resulting Status", $scope.status);
+	$scope.customCoordinates = false;
 
-// 		var humidityDanger = (parseFloat($scope.relative_humidity)/humidityLimit)*humidityModifier;
-// 		$scope.status -= humidityDanger;
-// 		console.log("resulting Status", $scope.status);
+	$scope.coordinateChange = function() {
+		console.log($scope.customCoordinates);
+		$scope.customCoordinates = !$scope.customCoordinates;
+	}
 
 
-// 		// DroneSpecs previously defined in $scope.calculateLaunchStatus
-// 		// add more algorhythmic analysis based on drones ****
-// 		// add plusses to the status/percentage based on droneSpecs
-// 		var weightGoal = 10000; //grams
-// 		var weightPlus = $scope.myDrone.weight/weightGoal //returns a number 0.0-1.0;
-// 		$scope.status += weightPlus;
-// 		console.log("resulting Status", $scope.status);
+	$scope.calculateStatus = function() {
+		var windMphLimit = 30;
+		var windGustLimit = 30;
+		var humidityLimit = 100;
+		var humidityModifier = .05;
+
+		$scope.status = 1;
+
+		// Modify the following to implement test conditions **************
+		// $scope.weather = "Partly Cloudy";
+		// $scope.wind_mph = 1.8;
+		// $scope.wind_gust_mph = "10.5";
+		// $scope.relative_humidity = "64%";
+		// $scope.temp_f = 63.1;
+		// console.log("weather", $scope.weather, "wind_mph", $scope.wind_mph, "gust", $scope.wind_gust_mph, "humidity", $scope.relative_humidity, "temperature", $scope.temp_f);
+
+		var windMphDanger = $scope.wind_mph/windMphLimit;
+		$scope.status -= windMphDanger;
+		// console.log("resulting Status", $scope.status);
+
+		var windGustDanger = parseFloat($scope.wind_gust_mph)/windGustLimit;
+		$scope.status -= windGustDanger;
+		// console.log("resulting Status", $scope.status);
+
+		var humidityDanger = (parseFloat($scope.relative_humidity)/humidityLimit)*humidityModifier;
+		$scope.status -= humidityDanger;
+		// console.log("resulting Status", $scope.status);
 
 
-// 		if ($scope.status>.75) {
-// 			console.log("Clear");
-// 			$rootScope.launchStatus = "Clear for takeoff!";
-// 		}
-// 		else if ($scope.status>.5) {
-// 			console.log("A little risky");
-// 			$rootScope.launchStatus = "A little risky";	
-// 		}
-// 		else if ($scope.status>.25) {
-// 			console.log("Very risky");	
-// 			$rootScope.launchStatus = "Very risky";
-// 		}
-// 		else {
-// 			console.log("No-go");
-// 			$rootScope.launchStatus = "No-Go";	
-// 		}
+		// DroneSpecs previously defined in $scope.calculateLaunchStatus
+		// add more algorhythmic analysis based on drones ****
+		// add plusses to the status/percentage based on droneSpecs
+		var weightGoal = 10000; //grams
+		var weightPlus = $scope.myDrone.weight/weightGoal //returns a number 0.0-1.0;
+		$scope.status += weightPlus;
 
-// /////////////////////THIS IS IT
-// 		$http.get("../geojson/reducedList.geo.json").success(function(data, status) {
-//       	for(var i = 0; i < data.features.length; i++)
-	      	// {
-	      	// 	if(gju.pointInPolygon({"type":"Point","coordinates":[-158.6178,59.2828]},
-        //          {"type":"Polygon", "coordinates": data.features[i].geometry.coordinates}))
-	      	// {
-	      	// 	console.log("hi");
-	      	// }
-	      	// else
-	      	// {
-	      	// 	console.log("no hi");
-	      	// }
-// ///////////////////////////////////////////////
-
-// 		$rootScope.$broadcast("changeLbackground");
-
-// 	}
+		$rootScope.inFlyZoneBl = false;
+		$scope.inFlyZoneStr = "No";
 
 
-// $scope.testFunction = function(flightTime, weight, operatingRange) {
-// 	// $scope.myDrone.weight = $scope.weight;
-// 	console.log("f", flightTime, "w", weight, "or", operatingRange);
-// }
+		
+	    // Within no flyzone function
+	    // change index.html with drone2.js to use function
+	    // test coordinates:
+		// [-158.6178, 59.2828] in noFlyZonePoly[1] return true
+		// [$long, $lat] W and S append negative
+
+		inFlyZoneBl = false;
+		$http.get("../geojson/reducedList.geo.json").success(function(data, status){
+
+			for( var i = 0; i < data.features.length; i++)
+			{
+				//coordinate to check if in no fly zone
+				//modify $root.longitude, $rootScope.latitude for test coordinates 
+				checkPoint = {"type": "Point", "coordinates": [-158.6178, 59.2828]};
+
+				//no-fly zone polygon coordinates
+				noFlyZonePoly = {"type": "Polygon", "coordinates": data.features[i].geometry.coordinates};
+
+				if(gju.pointInPolygon(checkPoint, noFlyZonePoly))
+				{
+					inFlyZoneBl = true;
+					console.log("in the no fly zone:", inFlyZoneBl);
+					break;
+				}
+
+				else
+				{
+					inFlyZoneBl = false;
+					console.log("in the no fly zone:", inFlyZoneBl);
+				}
 
 
-// });
+			}
+
+		});
+
+		console.log("in the no fly zone:", inFlyZoneBl);
 
 
-// // get geolocation going
+
+		// Within no flyzone function end
+		// comment: Let me know of bad test coordinates 
+		// and give me all the test coordinates you used.
+		// problem: line 319 called before function line 290 resulting in inFlyZone == false
+		// suggest change of $http.get to something else or wait till $http.get is complete. 
+
+
+
+		if ($scope.status>.75) {
+			console.log("Clear");
+			$rootScope.launchStatus = "Clear for takeoff!";
+		}
+		else if ($scope.status>.5) {
+			console.log("A little risky");
+			$rootScope.launchStatus = "A little risky";	
+		}
+		else if ($scope.status>.25) {
+			console.log("Very risky");	
+			$rootScope.launchStatus = "Very risky";
+		}
+		else {
+			console.log("No-go");
+			$rootScope.launchStatus = "No-Go";	
+		}
+
+		$rootScope.$broadcast("changeLbackground");      	
+
+      // });
+
+	$rootScope.$broadcast("changeLbackground");
+
+	}
+
+
+	$scope.testFunction = function(flightTime, weight, operatingRange) {
+		// $scope.myDrone.weight = $scope.weight;
+		console.log("f", flightTime, "w", weight, "or", operatingRange);
+	}
+
+
+
+
+
+
+});
+
+
+// get geolocation going
